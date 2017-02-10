16d78fd85948555b50f6bc194d66b53b1f6bc978
Pooh <onimusha.pooh@gmail.com>
2017-02-10 11:42:45 +0700

    first test

---
+++ index.js
@@ -0,0 +1,49 @@
+try {
+  require('dotenv').config()
+} catch (e) {
+  console.log('error', e)
+}
+const fs = require('fs')
+const http = require('http')
+const https = require('https')
+const express = require('express')
+
+const bodyParser = require('body-parser')
+const app = express()
+const api = express()
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('port', (process.env.PORT || 8080))
+
+// enable CORS
+api.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  next()
+})
+app.get('/',(req,res)=>{
+  res.status(200).send('server OK!!')
+})
+
+app.post('/api.ai',(req,res)=>{
+  var jsonRes = {"status": {
+                    "code": 206,
+                    "errorType": "partial_content",
+                    "errorDetails": "Webhook call failed. Status code 503. Error:503 Service Unavailable"
+                  }
+                }
+
+   res.status(503).json(jsonRes)             
+})
+
+// app.listen((process.env.PORT || 8080),(req,res)=>{
+
+// })
+var server = http.createServer(app).listen(process.env.PORT,function(){
+  console.log('server start poperly');
+});
