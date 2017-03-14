972fb3af1d17e7c603545c752174d6bc8da674ee
Pooh <onimusha.pooh@gmail.com>
2017-03-14 16:02:17 +0700

    update query string

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/oauth',(req,res)=>{
   var client_id = req.query.client_id
   var client_key = 'screencloud-test-service-only'  //new Date().getTime()
   var authorization_code = encrypt(client_id+'-'+client_key)
-  var redirect_url = req.query.redirect_uri+'?code='+authorization_code+'&result_code=SUCCESS'
+  var redirect_url = req.query.redirect_uri+'?code='+authorization_code+'&result_code=SUCCESS&state='+req.query.state
   
   res.redirect(redirect_url)
 })
