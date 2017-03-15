14bcd2f6388d36e4298c60b213f9f8333f450482
natp0ng <nattapong.kongmun@gmail.com>
2017-03-15 22:38:33 +0700

    test

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const express = require('express')
 
 const bodyParser = require('body-parser')
 const app = express()
-const exphbs = require('express-handlebars')
 
 // const api = express()
 var WebSocketServer = require('uws').Server
@@ -26,6 +25,8 @@ var googleWSConnections = {}
 var amazonWSConnections = {}
 var appsList = ['youtube','notice','message','nba','nfl','football','soccer','epl','premier league','time','weather','skynews','board','trello','slack','facebook','twitter','instagram','giphy','bbc',
 'cnn','techcrunch','stock','livenews','nasa']
+
+const exphbs = require('express-handlebars')
 
 function encrypt(text){
   var cipher = crypto.createCipher(algorithm,password)
