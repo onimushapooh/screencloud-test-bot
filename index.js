6e5026a86301e45ed13fe2124eeda7197be7f25d
Pooh <onimusha.pooh@gmail.com>
2017-03-16 11:57:43 +0700

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express = require('express')
 
 const bodyParser = require('body-parser')
 const app = express()
-// const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars')
 
 // const api = express()
 var WebSocketServer = require('uws').Server
