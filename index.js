49c71a8982963f5219b45442cd01a65c484145a7
Pooh <onimusha.pooh@gmail.com>
2017-03-17 18:18:38 +0700

    update html template

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ function decrypt(text){
 }
 
 function randomString(length) {
-    var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    // var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var chars = '0123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
     return result;
